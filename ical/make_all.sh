e577f33588191d50e82bec94c901c8074f60c595
fonzieyang <fonzieyang@gmail.com>
2015-10-25 08:24:00 +0800

    update cal

--- ical/make_all.sh
+++ ical/make_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo test > ~/Downloads/test
 
+pushd ~/Documents/fonzieyang.github.com/ical/
 osascript exportTyme.scpt
 mv ~/Downloads/tyme_for_cal.csv ./tyme_for_cal.csv
 python ConvCsvToiCal.py
@@ -8,3 +8,4 @@ python ConvCsvToiCal.py
 git add .
 git commit -m "update cal"
 git push
+popd
